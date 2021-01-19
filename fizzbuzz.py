9c32de69b5e153c9ab9da14580f3e2049f211da3
animeshraj1528 <74454644+animeshraj1528@users.noreply.github.com>
2021-01-19 19:11:41 +0530

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,24 @@
+#Made By Animesh Raj 
+#This was a popular puzzle in Python which many of the DEVs solved and had a experience in their DEV carreer.
+
+
+#Fizzzzzzzzzzzzz... 
+#Buzzzzzzzzzzzzzzz...  >_<
+for fizzbuzz in range(51):
+	
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print(fizzbuzz , "fizbuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print(fizzbuzz , "fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print(fizzbuzz , "buzz")
+        continue
+    print(fizzbuzz)
+    
+  
+  #You have downloaded it from the Internet...Grrr... Try it Urself and find more and solve them...
+  #Goodluck Buddy!! Have a nice DEV journey.  ^_^ 
+
+	
